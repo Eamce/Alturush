8cdbe079c9a3d7e3ccc44b36bf400cb2ba06b022
pjjy <pauljearicniones36@gmail.com>
2021-07-21 16:33:09 +0800

    p

--- lib/search.dart
+++ lib/search.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'db_helper.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -9,22 +10,24 @@ class Search extends StatefulWidget {
 class _Search extends State<Search> {
   final db = RapidA();
   final search = TextEditingController();
-  List searchProdData;
+  List searchProdData = [];
   bool searchLoading;
+  bool load;
 
   Future searchProd() async {
     searchLoading = true;
     var res = await db.searchProd(search.text,unitGroupId);
     if (!mounted) return;
     setState(() {
+      load = false;
       searchLoading = false;
       searchProdData = res['user_details'];
-      print(searchProdData);
     });
   }
 
   @override
   void initState() {
+    load = true;
     super.initState();
   }
 
@@ -56,6 +59,11 @@ class _Search extends State<Search> {
                 controller: search,
                 onChanged: (text) {
                   searchProd();
+                  if(search.text.length == 0){
+                    setState(() {
+                      searchProdData.clear();
+                    });
+                  }
                 },
                 prefix: Padding(
                   padding: EdgeInsets.only(left: 10),
@@ -66,6 +74,11 @@ class _Search extends State<Search> {
                   child: GestureDetector(
                     onTap: (){
                       search.clear();
+                      load = true;
+                      setState(() {
+                        searchProdData.clear();
+                      });
+                      print(load);
                     },
                       child: Icon(Icons.close_rounded,color: Colors.black54,)),
                 ),
@@ -85,21 +98,59 @@ class _Search extends State<Search> {
                 child: Text("Search",style: TextStyle(color: Colors.black),)
             )
           ],),
-        body: ListView.builder(
-            physics: BouncingScrollPhysics(),
-            shrinkWrap: true,
-            itemCount: searchProdData == null ? 0 : searchProdData.length,
-            itemBuilder: (BuildContext context, int index){
-              return InkWell(
-                onTap: (){
+        body: Scrollbar(
+          child: searchProdData.length !=0 || load == true ? ListView.builder(
+              physics: BouncingScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: searchProdData == null ? 0 : searchProdData.length,
+              itemBuilder: (BuildContext context, int index){
+                return InkWell(
+                  onTap: (){
 
-                },
-                child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-                    child: Text(searchProdData[index]['prod_name']),
-                ),
-              );
-            }
+                  },
+                  child:Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 15.0),
+                      child: Row(
+                        children:[
+                          Container(
+                            width: 30.0,
+                            height: 30.0,
+                            decoration: new BoxDecoration(
+                              image: new DecorationImage(
+                                image: new NetworkImage(searchProdData[index]['prod_image']),
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius: new BorderRadius.all(new Radius.circular(50.0)),
+                              border: new Border.all(
+                                color: Colors.black54,
+                                width: 0.5,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          Text(searchProdData[index]['prod_name'],style: TextStyle(fontSize: 12.0),)
+                        ],
+                      ),
+                  ),
+                );
+              }
+          ): Center(
+              child:Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    child: SvgPicture.asset("assets/svg/file.svg"),
+                  ),
+                  Text("No Result Found",style: TextStyle(color: Colors.black54,fontWeight: FontWeight.bold,fontSize: 20.0),),
+                  Text("We can't find any item matching your search",style: TextStyle(color: Colors.black54,),),
+                ],
+              ),
+          ),
         )
       ),
     );
